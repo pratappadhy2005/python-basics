c8ff89eb166726de97138f70c23a00106e48c802
Pratap Padhy <pratappadhy2005@gmail.com>
2025-05-06 20:38:43 +1000

    -Added modules and package functionality

---
+++ 5-Modules/package/subpackages/__init__.py
@@ -0,0 +1,5 @@
+'''
+This is a package for the python-basics project.
+'''
+
+__all__ = ["math_functions"]
---
+++ 5-Modules/test.py
@@ -0,0 +1,8 @@
+from package.subpackages.arithmetic import *
+from package.maths import *
+
+print(add(1, 2))
+print(subtract(5, 2))
+
+print(multiply(2, 3))
+print(divide(10, 2))
