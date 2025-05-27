dcf1a09c6d78502be0b3cb58feb43f47f16f215e
Pratap Padhy <pratappadhy2005@gmail.com>
2025-05-28 09:43:57 +1000

    -Added pythin module example

---
+++ 22-Module/module.py
@@ -0,0 +1,10 @@
+# Modules in python
+# It's single file
+
+# import mymodule
+# mymodule.say_hello('Pratap')
+# mymodule.say_hello('Raj')
+
+# Import specific fucntion and variable
+from mymodule import person1
+print(person1['name'])
---
+++ 22-Module/mymodule.py
@@ -0,0 +1,5 @@
+def say_hello(input):
+    return print(input)
+
+
+person1 = {"name": "pratap", "age": 21}
